0437bb500b24bba555c04f5fa4433e9025b1e478
dkeenan <keenan.deboissy@epitech.eu>
2024-12-16 14:49:46 +0100

    [UPDATE] customInformations (enhance error handling)

--- Core/Utils/customInformations.js
+++ Core/Utils/customInformations.js
@@ -1,7 +1,16 @@
 function showError(errorType, errorMessage, errorStack)
 {
     console.error(`\x1b[1m\x1b[38;2;255;0;0m[${errorType}] ${errorMessage}\x1b[0m`);
-    console.error(`\x1b[1m\x1b[38;2;255;0;0m[DETAILS] ${errorStack == null ? "Active debug mode to see the stack.\x1b[0m" : `${errorStack.toLowerCase() == "none" ? `No stack available.\x1b[0m` : `\x1b[0m${errorStack}`}`}`);
+    console.error(
+        `\x1b[1m\x1b[38;2;255;0;0m[DETAILS]` +
+        ` ${errorStack == null ?
+            `Active debug mode to see the stack.\x1b[0m` :
+            `${(typeof errorStack != "string" || errorStack.toLowerCase() == "none") ?
+                `No stack available.\x1b[0m` :
+                `\x1b[0m${errorStack}`
+            }`
+        }`
+    );
 }
 
 function showInfo(infoType, infoMessage)
